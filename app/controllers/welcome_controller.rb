8c3f61ebb70dba56c8887f6ef1810e8dd842e571
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-07 13:48:02 -0400

    m

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,11 @@
 class WelcomeController < ApplicationController
   def index
 	  @post = Post.new
-    if session[:coords].nil?
+	  @coords = session[:coords]
+    if coords.nil?
       setup_posts_of_the_world
 	  else
-	    @posts = Post.near(session[:coords],1,:order => {:created_at=>:desc})
+	    @posts = Post.near(coords,1,:order => {:created_at=>:desc})
 	    @location = session[:location]
 		end
   end
