9c822db5e8d5a5275a87200cbc6f957db832cedb
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-07 14:00:20 -0400

    map fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,10 @@ class WelcomeController < ApplicationController
   def index
 	  @post = Post.new
 	  @coords = session[:coords]
-    if coords.nil?
+    if @coords.nil?
       setup_posts_of_the_world
 	  else
-	    @posts = Post.near(coords,1,:order => {:created_at=>:desc})
+	    @posts = Post.near(@coords,1,:order => {:created_at=>:desc})
 	    @location = session[:location]
 		end
   end
