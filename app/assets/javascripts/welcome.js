84bd915ba86ec8e5fc9ab4b6a47c3fe0a80dc185
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-09 05:49:52 -0400

    added mood attr

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -26,15 +26,9 @@ $(function(){
     getPostsForLocation(world);
   });
 
-	function replace_class(affected_id,class_name) {
-	  $("#"+affected_id).removeClass();
-	  $("#"+affected_id).addClass(class_name);		
-  }
-
 	$("#post_mood").change(function() {
     var mood = $("#post_mood option:selected").text().toLowerCase();
-    var id = 'new_post_box';
-    replace_class(id,'post-'+mood);
+    $('#new_post_box').attr('mood',mood);
 	});
 });
 
