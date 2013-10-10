ad796f216d86e80d031ea3e8aeffa88f61ab7382
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-09 23:51:05 -0400

    map fixed

--- app/assets/javascripts/set_map.js
+++ app/assets/javascripts/set_map.js
@@ -1,7 +1,7 @@
 function setMap(lat,lon) {
 	var latlon = lat+","+lon;
 	var srcString = "http://maps.googleapis.com/maps/api/staticmap?center="
-	+latlon+"&zoom=12&size=185x185&sensor=false&markers=|"+latlon;
+	+latlon+"&zoom=12&size=210x210&sensor=false&markers=|"+latlon;
 	if (lat=="nil") srcString = "assets/tiles/globe.png";
   $("#map").attr("src", srcString);
 
@@ -30,8 +30,8 @@ function setMap(lat,lon) {
   $(".map-div").append(fullscreenlink);
 
   $(fullscreen).bind("mouseover", function(){
-      $(this).css("opacity", "1");
+    $(this).css("opacity", "1");
   }).bind("mouseout", function(){
-      $(this).css("opacity", "0.7");
+    $(this).css("opacity", "0.7");
   });
 }
