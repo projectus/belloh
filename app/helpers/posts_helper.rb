2a03783df20035b6a289c9749a04ebeb61c51598
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-16 02:19:34 -0400

    restyled

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+	def readable_time_ago(time)
+		tdiff = Time.now - time
+		days = (tdiff / 1.day).round
+		unless days == 0
+			return days.to_s + 'd'
+		end
+		hours = (tdiff / 1.hour).round
+	  unless hours == 0
+		  return hours.to_s + 'h'
+		end
+		minutes = (tdiff / 1.minute).round
+	  unless minutes == 0
+			return minutes.to_s + 'm'
+		end
+		seconds = (tdiff / 1.second).round
+		return seconds.to_s + 's'
+	end
 end
