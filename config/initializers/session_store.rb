4cb8e36edc8b1c6c89cf3b3519cf2a4a82cb5e4e
Eric Webster <quanta@Eric-Websters-MacBook-Pro.local>
2013-10-24 14:41:25 -0400

    m

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Belloh::Application.config.session_store :cookie_store, key: '_belloh_session'
+Belloh::Application.config.session_store :cookie_store, key: '_belloh_session', domain: '.belloh.com'
